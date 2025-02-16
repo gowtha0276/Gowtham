c226d7584cb6fa94e3210b8651e00c1102da4936
gowtha0276 <51858826+gowtha0276@users.noreply.github.com>
2025-02-17 02:58:28 +0530

    experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { AccordionComponent } from './components/accordion/accordion.component';
 import { HeaderComponent } from './components/header/header.component';
 import { Router } from '@angular/router';
+import { WorkexperienceComponent } from './components/workexperience/workexperience.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AccordionComponent,HeaderComponent],
+  imports: [RouterOutlet, AccordionComponent,HeaderComponent, WorkexperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
